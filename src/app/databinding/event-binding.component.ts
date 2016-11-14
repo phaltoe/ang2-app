a14e2a78a1e9d3575a839126aae1d492e5c50f25
phaltoe <reverse9@live.com>
2016-11-14 10:05:45 -0200

    two-way data binding on the way

--- src/app/databinding/event-binding.component.ts
+++ src/app/databinding/event-binding.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-event-binding',
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class EventBindingComponent {
+  @Output() clicked = new EventEmitter<string>();
   onClicked() {
-    alert('It Worked!')
+    this.clicked.emit('It works!');
   }
+
 }
